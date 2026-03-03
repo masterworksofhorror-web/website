6e67612be201cbec6eec89bb1c0a812ed3e88f51
Akash Gudi <akashgudi@Akashs-MacBook-Pro.local>
2026-03-03 12:19:05 -0800

    upd8: added routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/test" element={<div>Hello</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
