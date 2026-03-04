0f12cd254998cc05861c9a9ce2d606d8048c8bf1
Akash Gudi <akashgudi@Akashs-MacBook-Pro.local>
2026-03-03 16:32:24 -0800

    resquash to remove gifs

---
+++ src/components/trailer-section/TrailerSection.jsx
@@ -0,0 +1,16 @@
+import styles from "./trailer-section.module.css";
+export const TrailerSection = () => {
+  return (
+    <div className={styles.sectionContainer}>
+      <img src="/assets/placeholder.png" className={styles.trailerVideo} />
+      <div>
+        <h3 className={styles.copyHeading}>What is Masterworks of Horror</h3>
+        <p className={styles.copyBody}>
+          Masterworks of Horror is a fast paced real time card game based off
+          the literary works of such famous authors as H.P. Lovecraft, Edgar
+          Allen Poe, and many more.
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,11 +1,17 @@
+import { BattleSection } from "../components/battle-section/BattleSection";
+import { CraftSection } from "../components/craft-section/CraftSection";
 import { HeroSection } from "../components/hero-section/HeroSection";
 import { MenuBar } from "../components/menu-bar/MenuBar";
+import { TrailerSection } from "../components/trailer-section/TrailerSection";
 export const HomePage = () => {
   return (
     <>
       <div>
         <MenuBar />
         <HeroSection />
+        <TrailerSection />
+        <CraftSection />
+        <BattleSection />
       </div>
     </>
   );
