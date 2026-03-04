c2d0139f554896d0234a34d10cc207e8f1962841
Akash Gudi <akashgudi@Akashs-MacBook-Pro.local>
2026-03-03 19:07:29 -0800

    added signup section

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -6,25 +6,9 @@ export const HeroSectionDesktop = () => {
       <img src="/assets/lovecraft.png" className={styles.authorImage} />
       <div>
         <img src="/assets/logo.png" />
-        <p className={styles.signUp}> Sign Up Now</p>
+        <p className={styles.signUp}> Wishlist Now</p>
         <div className={styles.ctas}>
           <img src="/assets/steam-cta.png" className={styles.ctaLogo} />
-          <div className={styles.betaAccessBox}>
-            <p>Beta Access</p>
-            <div>
-              <img
-                src="/assets/apple-logo.png"
-                alt=""
-                className={styles.ctaLogo}
-              />
-              <img
-                src="/assets/google-play.png"
-                alt=""
-                className={styles.ctaLogo}
-                style={{ padding: "0 12px 0 0" }}
-              />
-            </div>
-          </div>
         </div>
       </div>
       <img src="/assets/poe.png" className={styles.authorImage} />
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -2,6 +2,7 @@ import { BattleSection } from "../components/battle-section/BattleSection";
 import { CraftSection } from "../components/craft-section/CraftSection";
 import { HeroSection } from "../components/hero-section/HeroSection";
 import { MenuBar } from "../components/menu-bar/MenuBar";
+import { SignupSection } from "../components/sign-up-section/SignupSection";
 import { TrailerSection } from "../components/trailer-section/TrailerSection";
 export const HomePage = () => {
   return (
@@ -12,6 +13,7 @@ export const HomePage = () => {
         <TrailerSection />
         <CraftSection />
         <BattleSection />
+        <SignupSection />
       </div>
     </>
   );
